207f9ff3ce9be875d693d429d864fc0552e63b80
Leandro Segovia <ldlsegovia@gmail.com>
2021-06-22 12:04:49 -0300

    chore(): update dependencies to play nice with rails 6
    replace travis with circle ci
    
    use ruby 2.7

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,26 +1,22 @@
-require_relative 'boot'
+require_relative "boot"
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
-require "sprockets/railtie"
+require "rails/all"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require "power_api"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-    config.active_record.sqlite3.represent_boolean_as_integer = true
+    config.load_defaults Rails::VERSION::STRING.to_f
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
