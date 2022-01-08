a528fe95b121ff65772d9be69905f109b46b42fd
Leandro Segovia <ldlsegovia@gmail.com>
2022-01-08 15:31:54 -0300

    style(): remove useless config from dummy routes

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,12 +1,2 @@
 Rails.application.routes.draw do
-  mount Rswag::Api::Engine => '/api-docs'
-  mount Rswag::Ui::Engine => '/api-docs'
-  scope path: '/api' do
-    api_version(module: 'Api::V2', path: { value: 'v2' }, defaults: { format: 'json' }) do
-    end
-
-    api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
-      resources :blogs
-    end
-  end
 end
